a3313312650b1d22431c246fdd4117400cc004fd
vitor4818 <vitorg.martins4818@gmail.com>
2025-03-21 17:12:06 -0300

    adicionando primeira parte do projeto

--- App.js
+++ App.js
@@ -1,10 +1,46 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState, useEffect } from 'react';
+import { DevToolsSettingsManager, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { TextInput } from 'react-native-web';
 
 export default function App() {
+  const [tarefas, setTarefas] = useState([])
+  const [tarefaNova, setTarefaNova] = useState("")
+
+  const handleAddTarefa = () =>{
+    setTarefas([...tarefas, tarefaNova])
+    setTarefaNova("")
+  }
+
+  useEffect(() => {
+    if (tarefas.length > 0) {
+      console.log('Tarefa adicionada: ', tarefas[tarefas.length - 1]); // Mostra a última tarefa no console
+    }
+  }, [tarefas]); // O useEffect depende de 'tarefas'
+  
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Bem vindo ao FiaPlanner!</Text>
+      <TextInput
+      placeholder='Digite a tarefa'
+      value={tarefaNova}
+      onChangeText={setTarefaNova}/>
+
+      <TouchableOpacity
+      onPress={() => 
+        handleAddTarefa()
+      }>
+        <Text>Adicionar Tarefa</Text>
+        </TouchableOpacity>
+
+        {tarefas.map((tarefa, index) => (
+    <Text key={index}>{tarefa}</Text>
+  ))}
+ 
+
+      
+
+
       <StatusBar style="auto" />
     </View>
   );
